6159214025d34c53425798c957e834d9c30f982d
Kiran121245 <giddalurikiransai1@gmail.com>
2024-01-12 16:35:01 +0530

    TestingPytest1

--- AWSTestExecutions/test_iam.py
+++ AWSTestExecutions/test_iam.py
@@ -4,9 +4,10 @@ from AWSTestCases.AWS_IAM_Test import aws_iam_check_if_policy_exist, aws_iam_che
 # Create IAM client
 iam = boto3.client('iam')
 
+
 def test_aws_iam_check_if_policy_exist():
     assert aws_iam_check_if_policy_exist(iam) == True
 
 
 def test_aws_iam_check_if_role_exist():
-    assert aws_iam_check_if_role_exist(iam) == True
+    assert aws_iam_check_if_role_exist(iam) == False
