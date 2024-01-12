f2e64d92a43f82430f38bb4f6ac1b5f2b9e951ce
Kiran121245 <giddalurikiransai1@gmail.com>
2024-01-12 19:17:53 +0530

    AddedSecretsInGithub

--- AWSTestExecutions/test_iam.py
+++ AWSTestExecutions/test_iam.py
@@ -1,7 +1,7 @@
 import boto3
 
 from AWSTestCases.AWS_IAM_Test import aws_iam_check_if_policy_exist, aws_iam_check_if_role_exist
-# Create IAM client
+# Create IAM client | this is just a modification doing for trigger
 iam = boto3.client('iam')
 
 
