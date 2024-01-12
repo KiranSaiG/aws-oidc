444026882340772ae960e0f4a7984a5fc75baad6
Kiran121245 <giddalurikiransai1@gmail.com>
2024-01-12 16:05:04 +0530

    CloudAWSPytest

--- AWSTestCases/AWS_IAM_Test.py
+++ AWSTestCases/AWS_IAM_Test.py
@@ -2,14 +2,15 @@ def aws_iam_check_if_policy_exist(iam):
     try:
         # Get a policy
         response = iam.get_policy(
-            PolicyArn='arn:aws:iam::aws:policy/AWSLambdaExecute'
+            PolicyArn='arn:aws:iam::791797855262:policy/service-role/AWSAthenaSparkRolePolicy-3qkpg41t'
         )
         print(response['Policy'])
     except Exception as e:
         print("error occured while invoking aws_iam_check_if_policy_exist and error is {}".format(e))
         return False
     else:
-         return True
+        return True
+
 
 def aws_iam_check_if_role_exist(iam):
     try:
@@ -22,4 +23,4 @@ def aws_iam_check_if_role_exist(iam):
         print("error occured while aws_iam_check_if_role_exist and error is {}".format(e))
         return False
     else:
-         return True
+        return True
